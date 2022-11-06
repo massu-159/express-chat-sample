23f7e83e39018a7a2963265c82e4f984ef7391a8
massu-159 <magjtwjp@gmail.com>
2022-11-06 10:17:47 +0900

    create socket

--- server.js
+++ server.js
@@ -6,8 +6,18 @@ const io = require("socket.io")(server);
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.sendFile(__dirname + "/index.html");
 });
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  
+  // ユーザーが送ったメッセージをサーバーが受け取る
+  socket.on("chat message", (msg) => {
+    // サーバーで受け取ったメッセージをユーザーに表示
+    io.emit("chat message", msg);
+  })
+})
 
 server.listen(PORT, () => {
   console.log("listening on 3000");
